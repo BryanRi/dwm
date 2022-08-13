e3371cd6401301f19a165661724e125438793144
Bryan Rinders <46681553+BryanRi@users.noreply.github.com>
2022-08-13 12:13:39 +0200

    update colors, keys, fontsize

--- config.def.h
+++ config.def.h
@@ -5,14 +5,14 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=9.5" };
-static const char dmenufont[]       = "monospace:size=9.5";
+static const char *fonts[]          = { "monospace:size=8.5" };
+static const char dmenufont[]       = "monospace:size=8.5";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#ebdbb2";
-static const char col_cyan[]        = "#974609";
-static const char col1[]            = "#ffffff";
+static const char col_gray3[]       = "#cccccc";
+static const char col_white[]       = "#eeeeee";
+static const char col_orange[]      = "#974609";
+static const char col1[]            = "#008282";
 static const char col2[]            = "#ffffff";
 static const char col3[]            = "#ffffff";
 static const char col4[]            = "#ffffff";
@@ -22,15 +22,15 @@ static const char col6[]            = "#ffffff";
 enum { SchemeNorm, SchemeCol1, SchemeCol2, SchemeCol3, SchemeCol4, 
 	SchemeCol5, SchemeCol6, SchemeSel }; /* color schemes */
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeCol1] = { col1,      col_gray1, col_gray2 },
-	[SchemeCol2] = { col2,      col_gray1, col_gray2 },
-	[SchemeCol3] = { col3,      col_gray1, col_gray2 },
-	[SchemeCol4] = { col4,      col_gray1, col_gray2 },
-	[SchemeCol5] = { col5,      col_gray1, col_gray2 },
-	[SchemeCol6] = { col6,      col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg         bg          border   */
+	[SchemeNorm] = { col_gray3, col_gray1,  col_gray2   },
+	[SchemeCol1] = { col_white, col1,       col1   },
+	[SchemeCol2] = { col2,      col_gray1,  col_gray2   },
+	[SchemeCol3] = { col3,      col_gray1,  col_gray2   },
+	[SchemeCol4] = { col4,      col_gray1,  col_gray2   },
+	[SchemeCol5] = { col5,      col_gray1,  col_gray2   },
+	[SchemeCol6] = { col6,      col_gray1,  col_gray2   },
+	[SchemeSel]  = { col_white, col_orange, col_orange  },
 };
 
 /* tagging */
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
@@ -81,7 +81,7 @@ static Key keys[] = {
     // Spawn basic programs
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_c,      spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 
     // Modify windows
